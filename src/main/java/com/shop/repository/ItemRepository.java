3b7e73367fe9b8ac4bedcc3f0f85ebd2117e2508
joyraptune <715sohappy@gmail.com>
2022-06-08 17:19:33 +0900

    init

---
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.shop.repository;
+
+import com.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+    List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+}
